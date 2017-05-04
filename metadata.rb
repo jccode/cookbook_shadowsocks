b5b229abe8c83b9e63dfc0a7dec16c7aa43f6c28
jessie <junchangchen@gmail.com>
2017-05-04 20:38:59 +0800

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures shadowsocks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'poise-service', '~> 1.5.1'
 
